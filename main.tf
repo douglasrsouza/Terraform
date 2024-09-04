41be6adbedac326b8c598218f96933febfda834f
douglasrsouza <douglasrimas@outlook.com>
2024-09-04 16:48:28 -0300

    commit inicial

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-05134c8ef96964280"
+  instance_type = "t2.micro"
+  key_name = "iac-douglas"
+  tags = {
+    Name = "Primeira instancia"
+  }
+}
